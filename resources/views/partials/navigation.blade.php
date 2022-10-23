be6eb8e374ceabf4e27e2db6ea4cb582ac7df6d7
PatriBlandon <blandonpatri26@gmail.com>
2022-10-22 20:57:36 -0600

    third comment navegacion

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -12,28 +12,7 @@
     <div class="container-fluid">
       <div class="collapse navbar-collapse"  id="navbarNav">
         <a class="navbar-brand" href="{{ route('welcome') }}">Bienvenidos</a>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('clinica') }}">Clinicas Medicas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('medicos') }}">Médicos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('contact') }}">Contactos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('about') }}">Acerca de</a>
-          </li>
-          {{--<li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">Registrate</a>
-          </li>--}}
-        </ul>
-        <form class="d-flex" role="registro">
-            <a href="{{ route('login') }}"><button class="btn btn-outline-success" type="submit">Registrate</button></a>
-        </form>
       </div>
-
     </div>
   </nav>
 <nav>

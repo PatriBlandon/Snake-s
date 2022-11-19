cde0fea9e6a0d167187d9860e470c26eb1402a59
PatriBlandon <blandonpatri26@gmail.com>
2022-11-19 16:20:35 -0600

    style: contenedores

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
  @section('meta-description', 'Acerca de meta description')
 
  @section('content')
-  <h1>Acerca de</h1>
+
     <div class="row justify-text-center">
         <div class="col col-sm-4">
             <h2>
